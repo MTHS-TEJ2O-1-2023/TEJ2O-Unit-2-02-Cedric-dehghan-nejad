42f6077bf14dd4c4f6e0c22335541f74cbf0cc75
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-15 12:43:47 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: cedric
+ * Created on: Sep 2023
+ * This program prints "hello MTHS"
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, MTHS')
