d97f1a6e2c61dda625f2960222e1a22792a5144e
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-15 21:08:06 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,9 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Cedric
+Created on: Sep 2023
+This module that shows "hello Mths"
 """
 
 from microbit import *
 
-
-display.scroll("Hello, World!")
+display.scroll("Hello, Mths!")
